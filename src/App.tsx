d5e4798dd8b4d9f97609735a90e3b7096d3d817e
firman maulana <firmanasgani37@gmail.com>
2024-04-18 21:50:53 +0700

    fixing forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import "./App.css";
-import { useState} from 'react'
-import { Layout, Row, Col, Input as InputAnt, Form as FormAntd, Button } from "antd";
-import { Formik, Form, Field } from "formik";
-import * as Yup from "yup";
+import { useState } from "react";
+import {
+  Layout,
+  Row,
+  Col,
+  Input as InputAnt,
+  Form as FormAntd,
+  Button,
+} from "antd";
+import Header from "./components/Header";
 
 const App = () => {
   const { Content } = Layout;
-  const [fullname, setFullname] = useState<string>("")
-  const [email, setEmail] = useState<string>("")
-  const [password, setPassword] = useState<string>("")
+  const [fullname, setFullname] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-  const RegisterForm = Yup.object().shape({
-    fullname: Yup.string()
-      .min(4, "Too Short!")
-      .max(50, "too Long")
-      .required("required"),
-      email: Yup.string().email('Invalid email').required('Required'),
-  });
+  const validateEmail = (email: string) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  const validateForm = () => {
+    try {
+      if (fullname === "") return false;
+      if (email === "") return false;
+      if(password === '') return false;
+      if (!validateEmail(email)) return false;
+      return true
+    } catch (err: any) {
+      console.error(`Error handling: ${err.message}`);
+    }
+  };
+
+  const handleSubmit = () => {
+    try {
+      const validate = validateForm();
+      if(validate) {
+        alert(`Success Added Data`)
+      }
+    } catch (err: any) {
+      console.error(`Error: handling ${err.message}`);
+    }
+  };
   return (
     <div className="App">
       <Layout>
-        <Content style={{ padding: "0 48px" }}>
+        <Content style={{ padding: "50px 48px" }}>
           <div
             style={{
               background: "white",
@@ -32,56 +61,106 @@ const App = () => {
           >
             <Row>
               <Col span={24}>
-                <h1 style={{ textAlign: "center", fontSize: 25 }}>
-                  Form Registration
-                </h1>
+                
+                <Header />
               </Col>
             </Row>
+          
             <FormAntd
               name="formRegister"
               labelCol={{ span: 4 }}
               wrapperCol={{ span: 20 }}
             >
-              <Formik
-                initialValues={{
-                  fullname: "",
-                  email: "",
-                  dateOfBirth: "",
-                }}
-                validationSchema={RegisterForm}
-                onSubmit={values => {
-                  console.log(values);
-                }}
+              <FormAntd.Item
+                label="Fullname"
+                name="fullname"
+                rules={[
+                  { required: true, message: "Please Input your fullname" },
+                ]}
               >
-                <Form>
-                  <FormAntd.Item
-                    label="Fullname"
-                    name="fullname"
-                    rules={[
-                      { required: true, message: "Please Input your fullname" },
-                    ]}
-                  >
-                  <InputAnt onChange={(e) => setFullname(e.target.value)}  />
-                  </FormAntd.Item>
-                  <FormAntd.Item
-                    label="Email"
-                    name="email"
-                    rules={[
-                      { required: true, message: "Please Input your email" },
-                      {
-                        type: 'email',
-                        message: 'The input is not valid E-mail!',
-                      },
-                      
-                    ]}
-                  >
-                  <InputAnt onChange={(e) => setEmail(e.target.value)}  />
-                  </FormAntd.Item>
-                  <FormAntd.Item>
-                  <Button type="primary" htmlType="submit">Submit</Button>
-                  </FormAntd.Item>
-                </Form>
-              </Formik>
+                <InputAnt onChange={(e) => setFullname(e.target.value)} />
+              </FormAntd.Item>
+              <FormAntd.Item
+                label="Email"
+                name="email"
+                rules={[
+                  { required: true, message: "Please Input your email" },
+                  {
+                    type: "email",
+                    message: "The input is not valid E-mail!",
+                  },
+                ]}
+              >
+                <InputAnt onChange={(e) => setEmail(e.target.value)} />
+              </FormAntd.Item>
+            
+
+
+              <FormAntd.Item
+                label="Street Address"
+                name="address"
+                rules={[
+                  { required: true, message: "Please Input your address" },
+                ]}
+              >
+                <InputAnt  />
+              </FormAntd.Item>
+              <FormAntd.Item
+                label="City"
+                name="city"
+                rules={[
+                  { required: true, message: "Please Input your City" },
+                ]}
+              >
+                <InputAnt  />
+              </FormAntd.Item>
+              <FormAntd.Item
+                label="State"
+                name="state"
+                rules={[
+                  { required: true, message: "Please Input your State" },
+                ]}
+              >
+                <InputAnt  />
+              </FormAntd.Item>
+              <FormAntd.Item
+                label="Zipcode"
+                name="zipcode"
+                rules={[
+                  { required: true, message: "Please Input your zipcode" },
+                ]}
+              >
+                <InputAnt  />
+              </FormAntd.Item>
+
+              <FormAntd.Item
+                label="Username"
+                name="username"
+                rules={[
+                  { required: true, message: "Please input your Username!" },
+                ]}
+              >
+                <InputAnt />
+              </FormAntd.Item>
+              <FormAntd.Item
+                label="Password"
+                name="password"
+                rules={[
+                  { required: true, message: "Please input your password!" },
+                ]}
+              >
+                <InputAnt.Password onChange={(e) => setPassword(e.target.value)} />
+              </FormAntd.Item>
+
+              <FormAntd.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  onClick={handleSubmit}
+                >
+                  Submit
+                </Button>
+              </FormAntd.Item>
             </FormAntd>
           </div>
         </Content>
