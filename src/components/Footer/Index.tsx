2a47c493abacb36479694ab60ef680304d0fdf2f
firman maulana <firmanasgani37@gmail.com>
2024-05-03 09:27:12 +0700

    remove target blank on footer

--- src/components/Footer/Index.tsx
+++ src/components/Footer/Index.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
 
         <>
             <p style={{textAlign: 'center', width: '100%', bottom: 0, position: "absolute", backgroundColor: 'black', color: 'white', padding: '5px'}}>
-              &copy;2024 <a href="https://firmanasgani.id" target="_blank">Firmanasgani.id</a>. All rights reserved.
+              &copy;2024 <a href="https://firmanasgani.id">Firmanasgani.id</a>. All rights reserved.
             </p>
         </>
     )
