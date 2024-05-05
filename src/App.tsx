013abd134287f0cf4c4ebf2e09257371e7384688
firman maulana <firmanasgani37@gmail.com>
2024-05-05 12:19:09 +0700

    change route /dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function Boostrap(){
           <Route path="/register" element={<Register />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
-          <Route path="/dashboard" element={<NotLoginYet />} />
+          <Route path="/dashboard" element={<Home />} />
           <Route path="*" element={<NotLoginYet />} />
         </Routes>
       </BrowserRouter>
